9296f832089de557f822fb5ab30217c323dd3c6a
alexisvisco <alexis.viscogliosi@outlook.fr>
2021-04-13 11:47:41 +0200

    .github: fix uses

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const github = require('@actions/github');
 
 try {
   console.log("this is a funky test")
-  core.setOutput("context", github.context)
-  core.setOutput("pr_name", core.getInput('pr_name'))
+  console.log(github.context)
+  console.log(core.getInput('pr_name'))
 } catch (ex) {
   core.setFailed(ex.message);
 }
