46c66973094d6458d9c67e7d3602343df55b4f68
alexisvisco <alexis.viscogliosi@outlook.fr>
2021-04-13 11:36:52 +0200

    .github: add workflow

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const github = require('@actions/github');
 
 
 try {
+  console.log("this is a funky test")
   core.setOutput("context", github.context))
   core.setOutput("pr_name", core.getInput('pr_name'))
 } catch (ex) {
